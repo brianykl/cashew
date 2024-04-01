bebc5a12db974676229c94111eadbe8f4b38a1b9
brian <brian.yikun.li@gmail.com>
2024-03-31 21:02:48 -0400

    gave up on generalizing db functions, going to copy over db functions for each microservice

--- services/user/models/users_db.go
+++ services/user/models/users_db.go
@@ -2,9 +2,34 @@ package models
 
 import (
 	"log"
+	// "testing"
 
-	. "github.com/brianykl/cashew/shared"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+func Connect() *gorm.DB {
+	dsn := "host=localhost user=postgres password=polarBear$02 dbname=cashew_db port=5432 sslmode=disable TimeZone=America/New_York"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	log.Println("connected to database successfully")
+	return db
+}
+
+func Insert(record interface{}) error {
+	db := Connect()
+	result := db.Create(record)
+	if result.Error != nil {
+		log.Printf("failed to insert %v", result.Error)
+		return result.Error
+	}
+
+	log.Printf("inserted successfully")
+	return nil
+}
 
 func Query(query interface{}, result interface{}) (interface{}, error) {
 	db := Connect()
